0e57771eb98605377a0dc8d80b0b20f394417f59
ambinayaka73380 <ambinayaka73380@gmail.com>
2026-02-04 14:15:40 +0530

    Initial Spring Boot project

---
+++ src/test/java/in/sort/NamNandiniApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamNandiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
